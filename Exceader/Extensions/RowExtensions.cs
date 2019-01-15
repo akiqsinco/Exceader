2a33611db15526bcc723f1c864b68e4373e72c1f
akiqsinco <akiqsinco@gmail.com>
2019-01-15 23:17:04 +0900

    Add row extension method.

--- Exceader/Extensions/RowExtensions.cs
+++ Exceader/Extensions/RowExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Exceader.Models;
+﻿using Exceader.Common;
+using Exceader.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,33 @@ namespace Exceader.Extensions
             }
 
             return Enumerable.Range(from, to - from).Select(i => row[i]);
+        }
+
+        public static IEnumerable<ICell> Range(this IRow row, string from, string to)
+        {
+            if (row == null)
+            {
+                throw new ArgumentNullException(nameof(row));
+            }
+
+            if (from == null)
+            {
+                throw new ArgumentNullException(nameof(from));
+            }
+
+            if (to == null)
+            {
+                throw new ArgumentNullException(nameof(to));
+            }
+
+            var start = ExcelNumber.ToNumerical(from);
+            var end = ExcelNumber.ToNumerical(to);
+            if (end < start)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return Enumerable.Range(start, end - start).Select(i => row[i]);
         }
     }
 }
